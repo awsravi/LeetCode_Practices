b4a2e97b592758d27b17ba1cc17215576fa17799
ravikumar <kp.ravikumar04@gmail.com>
2025-02-25 16:19:07 +0530

    Initial commit

---
+++ src/main/java/com/awsravi/leetcode_practices/LeetCodePracticesApplication.java
@@ -0,0 +1,13 @@
+package com.awsravi.leetcode_practices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodePracticesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetCodePracticesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/awsravi/leetcode_practices/LeetCodePracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsravi.leetcode_practices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodePracticesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
